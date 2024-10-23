1a9f9b1714735564c10b316875e5f1d97ef55032
Daniel O'Brien <dobrien@atenik.xyz>
2024-10-23 16:44:04 +0100

    Using react-query to manage the browser-based cache.

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -1,10 +1,18 @@
 export const getMovies = () => {
     return fetch(
-        `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&page=1`
-    )
-        .then(res => res.json())
-        .then(json => json.results);
-};
+      `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+    ).then((response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+        throw error
+    });
+  };
 
 export const getMovie = id => {
     return fetch(
@@ -14,13 +22,21 @@ export const getMovie = id => {
 
 export const getGenres = () => {
     return fetch(
-        "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
+      "https://api.themoviedb.org/3/genre/movie/list?api_key=" +
         process.env.REACT_APP_TMDB_KEY +
         "&language=en-US"
-    )
-        .then(res => res.json())
-        .then(json => json.genres);
-};
+    ).then( (response) => {
+      if (!response.ok) {
+        return response.json().then((error) => {
+          throw new Error(error.status_message || "Something went wrong");
+        });
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+   });
+  };
 
 export const getMovieImages = (id) => {
     return fetch(
