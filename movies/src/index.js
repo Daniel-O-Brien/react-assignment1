7f5bde97580b9c73400e710492d41c6a4d5aff40
Daniel <20101948@mail.wit.ie>
2024-11-15 19:43:15 +0000

    Added Trending static endpoint

--- movies/src/index.js
+++ movies/src/index.js
@@ -11,6 +11,7 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
 import AddMovieReviewPage from './pages/addMovieReviewPage'
 import UpcomingMoviesPage from './pages/upcomingMoviesPage'
+import TrendingMoviesPage from './pages/trendingMoviesPage'
 
 
 const queryClient = new QueryClient({
@@ -37,6 +38,7 @@ const queryClient = new QueryClient({
               <Route path="*" element={ <Navigate to="/" /> } />
               <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
               <Route path="/movies/upcoming" element={ <UpcomingMoviesPage /> } />
+              <Route path="/movies/trending" element={ <TrendingMoviesPage /> } />
             </Routes>
           </MoviesContextProvider>
         </BrowserRouter>
